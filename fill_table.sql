fc8b5c2a09f32aca308989b256be76e11b98d092
Vladimir Chekhlov <chekhlov51@gmail.com>
2022-04-21 01:41:24 +0300

    Final step done

--- fill_table.sql
+++ fill_table.sql
@@ -16,8 +16,8 @@ INSERT INTO outcome_type(name)
 SELECT DISTINCT outcome_type FROM animals;
 
 
-INSERT INTO new_animals(age_upon_outcome,
-                        animal_id,
+INSERT INTO new_animals(animal_id,
+                        age_upon_outcome,
                         name,
                         date_of_birth,
                         outcome_month,
@@ -30,24 +30,24 @@ INSERT INTO new_animals(age_upon_outcome,
                         outcome_type_id
                         )
 SELECT
-    a.age_upon_outcome,
     a.animal_id,
+    a.age_upon_outcome,
     a.animals.name,
     a.date_of_birth,
     a.outcome_month,
     a.outcome_year,
-    atp.id as type_id,
     ab.id as breed_id,
     ac1.id as color1_id,
     ac2.id as color2_id,
+    atp.id as type_id,
     ost.id as outcome_subtype_id,
     ot.id as outcome_type_id
 
 
 FROM animals a
 LEFT JOIN animal_breed ab ON ab.name = a.breed
-LEFT JOIN animal_type atp ON a.animal_type = atp.name
 LEFT JOIN animal_color ac1 ON ac1.name = a.color1
 LEFT JOIN animal_color ac2 ON ac2.name = a.color2
+LEFT JOIN animal_type atp ON a.animal_type = atp.name
 LEFT JOIN outcome_subtype ost ON ost.name = a.outcome_subtype
 LEFT JOIN outcome_type ot ON ot.name = a.outcome_type
